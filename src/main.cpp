91c43ce00893e63a2a77e2f85ccfbb0f683d062d
Josh F <jjjoshua287@gmail.com>
2026-01-27 21:19:13 -0500

    refactor: Move test function above main and adjust structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include "DistroDetector.h"
 
+void test(DistroInfo info, DistroConfig cfg) {
+    // Print Results --- Testing Purposes
+    std::cout << "-----    DistroDetector Test    -----" << std::endl;
+    std::cout << "Raw ID:          " << info.id << std::endl;
+    std::cout << "Like:            " << info.id_like << std::endl;
+    std::cout << "-------------------------------------" << std::endl;
+    std::cout << "Package Manager: " << cfg.manager << std::endl;
+    std::cout << "Command Prefix:  " << cfg.cmd << std::endl;
+}
+
 int main(int argc, char* argv[]) {
     DistroInfo info = DistroDetector::getDistroInfo();
     DistroConfig cfg = DistroDetector::getDistroConfig(info);
@@ -9,14 +19,5 @@ int main(int argc, char* argv[]) {
         std::cerr << "Error: Unsupported Linux Distribution." << std::endl;
         return 1;
     }
-
-    // Print Results --- Testing Purposes
-    std::cout << "-----    DistroDetector Test    -----" << std::endl;
-    std::cout << "Raw ID:          " << info.id << std::endl;
-    std::cout << "Like:            " << info.id_like << std::endl;
-    std::cout << "-------------------------------------" << std::endl;
-    std::cout << "Package Manager: " << cfg.manager << std::endl;
-    std::cout << "Command Prefix:  " << cfg.cmd << std::endl;
-
     return 0;
 }
