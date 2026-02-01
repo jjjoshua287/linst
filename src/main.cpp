763a6cf76e6a6c95715605a7d61fdeb1747b1718
Josh F <jjjoshua287@gmail.com>
2026-02-01 16:14:57 -0500

    refactor: remove unused test function from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,6 @@
 #include "DistroDetector.h"
 #include "PackageResolver.h"
 
-void test(DistroInfo info, DistroConfig cfg) {
-    // Print Results --- Testing Purposes
-    std::cout << "-----    DistroDetector Test    -----" << std::endl;
-    std::cout << "Raw ID:          " << info.id << std::endl;
-    std::cout << "Like:            " << info.id_like << std::endl;
-    std::cout << "-------------------------------------" << std::endl;
-    std::cout << "Package Manager: " << cfg.manager << std::endl;
-    std::cout << "Command Prefix:  " << cfg.cmd << std::endl;
-}
-
 void buildCommand(std::vector<std::string> names, std::string prefix) {
     std::cout << prefix << " ";
     for (const auto& name : names) {
