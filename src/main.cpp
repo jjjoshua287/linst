be02fd952a3aa566c1ba88f3b0c382d42a06e273
Josh F <jjjoshua287@gmail.com>
2026-02-01 16:12:59 -0500

    feat(): allow user selected packages as command line arguments
    - removed hard-coded test cases

--- src/main.cpp
+++ src/main.cpp
@@ -23,18 +23,20 @@ void buildCommand(std::vector<std::string> names, std::string prefix) {
 }
 
 int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: linst package1 package2 package3 ..." << std::endl;
+        return 1;
+    }
+
     DistroInfo info = DistroDetector::getDistroInfo();
     DistroConfig cfg = DistroDetector::getDistroConfig(info);
     if (cfg.id == "unsupported") {
         std::cerr << "Error: Unsupported Linux Distribution." << std::endl;
         return 1;
     }
-    std::vector<std::string> usrPkg; // temp testing vector
-    usrPkg.push_back("firefox");
-    usrPkg.push_back("git");
-    usrPkg.push_back("docker");
-    std::vector<std::string> names = PackageResolver::resolvePackages(usrPkg, cfg);
-    
+
+    std::vector<std::string> userPkgs(argv + 1, argv + argc);
+    std::vector<std::string> names = PackageResolver::resolvePackages(userPkgs, cfg);
     buildCommand(names, cfg.cmd);
 
     return 0;
